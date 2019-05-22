1c61dda5a54972985d93770598ee7d92526b60f5
thekosa <michalkosieradzki95@gmail.com>
2019-05-22 16:34:09 +0200

    rozgrzebanie tablicy

--- src/pl/projekt/szachy/MainWindowController.java
+++ src/pl/projekt/szachy/MainWindowController.java
@@ -2,16 +2,17 @@ package pl.projekt.szachy;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainWindowController implements Initializable {
-    @FXML
-    Label newGameButton;
-    @FXML
-    Label loadGameButton;
+    @FXML Button newGameButton;
+    @FXML Button loadGameButton;
+    @FXML Button saveGameButton;
+
 
 //todo jezeli bedzie opcja new game - przekazujemy do gamestate zeby odczytywac z odpowiedniego pliku,
 // jezeli wybierzemy co innego to z tego innego
@@ -21,18 +22,24 @@ public class MainWindowController implements Initializable {
 
     }
 
-    public void newGameButtonClicked() {
-        GameState gameState;
-        gameState = new GameState(true);
-        for (Figure figure : gameState.getGameState()) {
+    public void newGameButtonAction() {
+        System.out.println("nowa gra");
+       // GameState gameState;
+     //   gameState = new GameState(true);
+       // for (Figure figure : gameState.getGameState()) {
             //wyciagamy kazda informacje po kolei:
             // kordynatory wpakowujemy tam gdzie bedzie miejsce okreslajace umiejscowienie obrazka,
             // kolor bedzie definiowal jaki kolor
             // nazwa - do nazwy bedziemy dodawac ".jpg" i za pomoca String figureJPGName bedziemy wywolywac obrazek
-        }
+      //  }
     }
 
-    public void loadGameButtonClicked() {
+    public void loadGameButtonAction() {
+        System.out.println("Wczytaj gre");
         //okno sie wyswietla z mozliwoscia wyboru poprzedniej gry
+    }
+
+    public void saveGameButtonAction(){
+        System.out.println("Zapisz gre");
     }
 }
