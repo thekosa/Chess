431340cd421090734edc70b7c984825dbb1ea9fc
thekosa2 <michal.kosieradzki@post.pl>
2019-06-10 12:11:10 +0200

    rospoczecie pracy nad possibleMoves

--- src/pl/projekt/szachy/Knight.java
+++ src/pl/projekt/szachy/Knight.java
@@ -3,7 +3,27 @@ package pl.projekt.szachy;
 import javafx.scene.paint.Color;
 
 public class Knight extends Figure {
+    private boolean[][] possibleMoves = new boolean[Board.getN()][Board.getN()];
+
     Knight(Color color) {
         super(color, "knight");
+        setPossibleMoves();
+    }
+
+    public void setPossibleMoves() {
+        resetPossibleMoves();
+
+    }
+
+    public boolean[][] getPossibleMoves() {
+        return possibleMoves;
+    }
+
+    private void resetPossibleMoves() {
+        for (int i = 0; i < Board.getN(); i++) {
+            for (int j = 0; j < Board.getN(); j++) {
+                possibleMoves[i][j] = false;
+            }
+        }
     }
 }
